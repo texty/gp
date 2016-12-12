3a13566ebfb916b7945bafefb5953ba644c609c2
An-21 <andriy.gazin@gmail.com>
2016-12-12 16:29:53 +0200

    add illegal points

--- js/map.js
+++ js/map.js
@@ -30,3 +30,44 @@ $.getJSON("data/gp.geojson", function(data){
            }
          }).addTo(map);
        });
+
+ //add licenses points
+ $.getJSON("data/gp_illegal.geojson", function(data){
+
+ //define style
+   function style(feature) {
+     return {
+       radius: 6,
+       fillColor: '#d3c756',
+       color: '#FEFFEA',
+       weight: 2,
+       opacity: 1,
+       fillOpacity: 0.75
+       };
+     }
+
+     L.geoJson(data, {
+           pointToLayer: function(feature, latlng){
+             return L.circleMarker(latlng, style(feature));
+            },
+            onEachFeature: function (feature, layer) {
+              layer.bindPopup('<b>' + feature.properties.name.toUpperCase() + '</b>' + '<br><hr>' + feature.properties.address);
+            }
+          }).addTo(map);
+        });
+
+
+//add legend
+var legend = L.control({position: 'bottomleft'});
+
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        colors = ['#d3c756', '#327C8C'],
+        labels = ['місця без ліцензії', 'місця з ліцензією'];
+    for (var i = 0; i < colors.length; i++) {
+        div.innerHTML += '<i class="fa fa-circle" aria-hidden="true" style="color:' + colors[i] + '"></i> ' + labels[i] + '<br>';
+          }
+    return div;
+    };
+
+map.addControl(legend);
